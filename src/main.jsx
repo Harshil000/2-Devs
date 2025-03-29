fbbc62995ff83714a22dca8aad99c4dee8b31cd9
Harshil000 <techshock01@gmail.com>
2025-03-29 13:51:37 +0530

    authentication functionality added and created little bit of homepage and configured react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-kbdutritxkhecdv7.us.auth0.com"
+      clientId="17POntv0Fu1jrdWq5CkMl998iSzNPywA"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Moon as Balloon } from 'lucide-react';
+import { useAuth0 } from "@auth0/auth0-react";
+import '../styles/Home.css'
+
+const Home = () => {
+
+    const { user, isAuthenticated, isLoading, loginWithRedirect, logout } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+
+    return (
+        <>
+            <div className='w-full h-screen bg-slate-950'>
+                <nav className='navbar'>
+                    <div className="flex items-center space-x-2 cursor-pointer">
+                        <Balloon className="text-blue-400" size={32} />
+                        <span className="text-blue-400 text-2xl font-bold">Balloon Pop!</span>
+                    </div>
+                    <div className='text-blue-400 text-xl font-bold'>
+                        BOOM X BAM
+                    </div>
+                    {!isAuthenticated ? <div onClick={() => loginWithRedirect()} className='loginBTN'>
+                        Login
+                    </div> : <div className='flex flex-col space-x-2 gap-2'>
+                        <span className='text-blue-400'>Hello , <span className='text-green-300'> {user.name} </span></span>
+                        <span onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className='cursor-pointer bg-red-500 hover:bg-red-700 transition-all w-fit px-2 text-white rounded-md'>LogOut</span>
+                    </div>}
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default Home
