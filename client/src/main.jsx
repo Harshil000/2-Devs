a25a8e22f4017afafe836074e38b87377733125c
Harshil000 <techshock01@gmail.com>
2025-04-21 17:58:42 +0530

    .env file implimentation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,10 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Auth0Provider
-      // domain={import.meta.env.DOMAIN}
-      domain="dev-kbdutritxkhecdv7.us.auth0.com"
-      // clientId={import.meta.env.CLIENT_ID}
-      clientId="17POntv0Fu1jrdWq5CkMl998iSzNPywA"
+      domain={import.meta.env.VITE_DOMAIN}
+      // domain="dev-kbdutritxkhecdv7.us.auth0.com"
+      clientId={import.meta.env.VITE_CLIENT_ID}
+      // clientId="17POntv0Fu1jrdWq5CkMl998iSzNPywA"
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
