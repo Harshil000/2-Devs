f66f61da987e91d6213f537afaea2333d297912a
Harshil000 <techshock01@gmail.com>
2025-04-21 11:19:17 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
 import GameScreen from './pages/GameScreen'
-import { useState } from 'react'
 
 function App() {
-
-    const [highScore, setHighScore] = useState(0)
 
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <Home highScore={highScore} />,
+            element: <Home/>,
         },
         {
             path: '/play',
-            element: <GameScreen setHighScore={setHighScore} />
+            element: <GameScreen/>
         }
     ])
 
