5ba86ce084439a64c80fc1f42314bcd2c0f1d554
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-04-21 18:06:28 +0530

    Attempt to solve DB problem

--- server/db.js
+++ server/db.js
@@ -1,8 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-const dbConnection = mongoose.connect(process.env.MONGO_URI).then(()=> {
-  console.log("Connected to DB")
-})
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+}
 
 module.exports = dbConnection
--- server/server.js
+++ server/server.js
@@ -1,55 +1,69 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const db = require("./db")
+const connectDB = require("./db")
 const userModel = require("./models/user")
 const uuid4 = require("uuid4")
+require("dotenv").config()
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-    res.send("src")
+  res.send("src")
 })
 
+const startServer = async () => {
+  await connectDB()
 
-app.post("/save-user", async (req, res) => {
+  app.post("/save-user", async (req, res) => {
     const { name, email } = req.body
     const findUser = await userModel.findOne({ email: email })
     if (findUser) {
-        return res.status(200).send(JSON.stringify({ "status": "200", "player": findUser }));
+      return res
+        .status(200)
+        .send(JSON.stringify({ status: "200", player: findUser }))
     }
     const newUser = await userModel.create({
-        name,
-        email,
-        highscore: 0,
-        uuid4: uuid4()
+      name,
+      email,
+      highscore: 0,
+      uuid4: uuid4(),
     })
-    res.send(JSON.stringify({ "status": "200", "player": newUser }));
-})
+    res.send(JSON.stringify({ status: "200", player: newUser }))
+  })
 
-app.post("/get-user" , async (req, res) => {
+  app.post("/get-user", async (req, res) => {
     const { uuid4 } = req.body
     const findUser = await userModel.findOne({ uuid4: uuid4 })
     if (findUser) {
-        return res.status(200).send(JSON.stringify({ "status": "200", "player": findUser }));
+      return res
+        .status(200)
+        .send(JSON.stringify({ status: "200", player: findUser }))
     }
-    res.send(JSON.stringify({ "status": "404" }))
-})
+    res.send(JSON.stringify({ status: "404" }))
+  })
 
-app.post("/UpdateScore", async (req, res) => {
-    const result = await userModel.updateOne({email: req.body.email}, {highscore: req.body.score})
+  app.post("/UpdateScore", async (req, res) => {
+    await userModel.updateOne(
+      { email: req.body.email },
+      { highscore: req.body.score }
+    )
     res.send("100")
-})
+  })
 
-app.get("/leaderboard", async (req, res) => {
-    let userData = await userModel.find({});
+  app.get("/leaderboard", async (req, res) => {
+    let userData = await userModel.find({})
     userData.sort((a, b) => b.highscore - a.highscore)
     userData = userData.slice(0, 3)
-    res.send(JSON.stringify(userData));
-})
+    res.send(JSON.stringify(userData))
+  })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`)
-})
+  const PORT = process.env.PORT || 5000
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+  })
+}
+
+startServer()
